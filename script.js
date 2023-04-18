26b6035713d816a99845f0ec266e38ec50648e66
wygrajr <wycliffe.graftonjr@gmail.com>
2023-04-18 19:19:34 -0400

    Added generatePassword function that prompts questions to user.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var generatePassword="";
+
+  var howManyChars=prompt("How many characters would you like to use in the password?");
+  var wantSpecialChars=confirm("Would you like to include special characters?");
+  var wantUpperChars=confirm("Would you like to include upper case characters?");
+  var wantLowerChars=confirm("Would you like to include lower case characters?");
+  var wantNumbersChars=confirm("Would you like to include numbers?");
+
+  return generatePassword;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
