ad8103588b413a8c8941fdf0540d4686f294e674
wygrajr <wycliffe.graftonjr@gmail.com>
2023-04-18 21:05:20 -0400

    Added character arrays - Speacial, Upper, Lower, and Numbers

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
   var generatePassword="";
 
+  var charSpecial=[" ","!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
+  var charUpper=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var charLower=["abcdefghijklmnopqrstuvwxyz"];
+  var charNumbers=[0,1,2,3,4,5,6,7,8,9];
+
   var howManyChars=prompt("How many characters would you like to use in the password?");
   var wantSpecialChars=confirm("Would you like to include special characters?");
   var wantUpperChars=confirm("Would you like to include upper case characters?");
@@ -11,6 +16,15 @@ function generatePassword(){
   var wantNumbersChars=confirm("Would you like to include numbers?");
 
   return generatePassword;
+}
+
+function charLength(){
+  var min=math.min(8);
+  var max=math.max(128);
+
+  if (min<=howManyChars<=max){
+
+  }
 }
 // Write password to the #password input
 function writePassword() {
