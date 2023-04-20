52540c0c19e174cbd65aa0f3cfa4ad61da77ce0a
wygrajr <wycliffe.graftonjr@gmail.com>
2023-04-20 18:48:50 -0400

    Seperated characters in Upper and Lower array

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function generatePassword(){
   var generatePassword="";
   
 
-  var howManyChars=prompt("How many characters would you like to use in the password?");
+  var howManyChars=parseInt(prompt("How many characters would you like to use in the password?"));
   if (8<=howManyChars<=128){
-    passwordLength=howManyChars
+   var passwordLength=howManyChars;
   }
 
   var wantSpecialChars=confirm("Would you like to include special characters?");
@@ -19,10 +19,10 @@ function generatePassword(){
   var charSpecial=[" ","!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
   var randomCharSpecial=getRandomValueFromArray(charSpecial);
 
-  var charUpper=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var charUpper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var randomCharUpper=getRandomValueFromArray(charUpper);
 
-  var charLower=["abcdefghijklmnopqrstuvwxyz"];
+  var charLower=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var randomCharLower=getRandomValueFromArray(charLower);
 
   var charNumbers=[0,1,2,3,4,5,6,7,8,9];
