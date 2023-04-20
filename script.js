b75dd22e086a3004b9e6c0ec5f408d57202a6eef
wygrajr <wycliffe.graftonjr@gmail.com>
2023-04-20 14:45:09 -0400

    Added random character selector for each option

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
   var generatePassword="";
 
-  var charSpecial=[" ","!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
-  var charUpper=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-  var charLower=["abcdefghijklmnopqrstuvwxyz"];
-  var charNumbers=[0,1,2,3,4,5,6,7,8,9];
-
   var howManyChars=prompt("How many characters would you like to use in the password?");
   var wantSpecialChars=confirm("Would you like to include special characters?");
   var wantUpperChars=confirm("Would you like to include upper case characters?");
   var wantLowerChars=confirm("Would you like to include lower case characters?");
   var wantNumbersChars=confirm("Would you like to include numbers?");
+
+  var charSpecial=[" ","!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
+  var randomCharSpecial=getRandomValueFromArray(charSpecial);
+
+  var charUpper=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var randomCharUpper=getRandomValueFromArray(charUpper);
+
+  var charLower=["abcdefghijklmnopqrstuvwxyz"];
+  var randomCharLower=getRandomValueFromArray(charLower);
+
+  var charNumbers=[0,1,2,3,4,5,6,7,8,9];
+  var randomCharNumbers=getRandomValueFromArray(charNumbers);
 
   return generatePassword;
 }
