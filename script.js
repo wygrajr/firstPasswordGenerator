5b794cfcc0f826676c541ccf08b692c500d461aa
wygrajr <wycliffe.graftonjr@gmail.com>
2023-04-20 18:12:25 -0400

    Added character randomizer. Added character limiter

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
   var generatePassword="";
+  var characters=[];
 
   var howManyChars=prompt("How many characters would you like to use in the password?");
+  if (8<=howManyChars<=128){
+    passwordLength=howManyChars
+  }
+  
   var wantSpecialChars=confirm("Would you like to include special characters?");
   var wantUpperChars=confirm("Would you like to include upper case characters?");
   var wantLowerChars=confirm("Would you like to include lower case characters?");
@@ -23,7 +28,7 @@ function generatePassword(){
   var charNumbers=[0,1,2,3,4,5,6,7,8,9];
   var randomCharNumbers=getRandomValueFromArray(charNumbers);
 
-  var characters=[];
+ 
   if (wantSpecialChars === true) {
     addSpecialChars=characters.concat(randomCharSpecial);
   }
@@ -60,21 +65,20 @@ function generatePassword(){
     addNumbersChars=characters.concat(randomCharLower);
   }
 
+  for (var i=0;i<password.Length;i++){
+    password+=getRandomValueFromArray(addNumbersChars)
+  }
+
   return generatePassword;
 }
 
-function charLength(){
-  var min=math.min(8);
-  var max=math.max(128);
 
-  if (min<=howManyChars<=max){
 
-  }
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
